27defcdd50c5550648c186667b90c6fa90557484
ciaran <ciaranjotter@gmail.com>
2022-07-19 17:13:16 +0200

    Initial workings og the block creator with a working style system

--- front-end/script.js
+++ front-end/script.js
@@ -1,3 +1,65 @@
 const admin = localStorage.getItem("admin")
 
 console.log(admin)
+
+
+
+class Block {
+    constructor() {
+        this.components = [];
+        this.baseContainer = "<div>This is a test</div>"
+        this.editMenu = document.createElement("div");
+        this.editMenu.id = "editMenu"
+        this.Styles = {
+            backgroundColor: "#000000",
+            opacity: "0.75",
+            position: 'fixed',
+            left: "0",
+            top: "0",
+            border: "1px solid blue",
+            width: "100%",
+            height: "100%",
+        }
+    }
+
+    test() {
+        console.log("You created a new block")
+    }
+
+    showEditPage() {
+        document.body.appendChild(this.editMenu)
+
+
+        // let temp = "backgroundColor"
+        // this.editMenu.style[`${temp}`] = "#000000"
+
+
+        for (var property in this.editStyles) {
+            console.log(typeof(property))
+            console.log(property, this.editStyles[`${property}`]);
+            this.editMenu.style[`${property}`] = this.editStyles[`${property}`]
+        }
+        this.editMenu.innerHTML = "This is a test";
+        
+    }
+}
+
+class Component {
+    constructor() {
+
+    }
+}
+
+class VideoComponent extends Component{
+    constructor() {
+        super();
+    }
+}
+
+window.newBlock = newBlock;
+
+function newBlock() {
+    const block = new Block();
+    block.test();
+    block.showEditPage();
+} 
